a9aa53d4f7a7400acec78491a6a8cf7883d5a9a4
Quoc Khanh <trandamquockhanh123@gmail.com>
2026-03-11 16:14:44 +0700

    add master file

---
+++ master.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Danh sách các file cần quét
+FILES=(
+    "src/benchmark/RespLLM/model.py"
+    "/home/tran-dam-quoc-khanh/Documents/DIEM_MY/OPERA/src/benchmark/RespLLM/RespLLM.py"
+)
+
+echo "--- Checking tokens ---"
+
+for FILE in "${FILES[@]}"; do
+    # Kiểm tra file có tồn tại không trước khi quét
+    if [ -f "$FILE" ]; then
+        if grep -q "redacted" "$FILE"; then
+            echo "--------------------------------------------------------"
+            echo "have not include token in the folowing files:"
+            echo "-> $FILE"
+            echo "--------------------------------------------------------"
+            echo "You have to write token like the tutorial."
+            # echo "Vui lòng kiểm tra lại os.environ['WANDB_API_KEY'] hoặc file .env"
+            exit 1 # Ngắt toàn bộ script bash với mã lỗi
+        fi
+    else
+        echo "Cảnh báo: Không tìm thấy file $FILE để quét."
+    fi
+done
+
+echo "Token valid. Continue."
+
+# bash scripts/setup.sh
+source scripts/setup_env.sh
+bash scripts/run_audio_label.sh
+bash scripts/run_wavelet.sh
