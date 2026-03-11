16c43ea0389fbb50c18921c4b7fdde9a000a5909
Quoc Khanh <trandamquockhanh123@gmail.com>
2026-03-11 16:53:57 +0700

    update

--- scripts/run_audio_label.sh
+++ scripts/run_audio_label.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BATCH_SIZE=${1:-16}
+
 mkdir -p logs_1
 
 # no audio label
@@ -8,9 +10,9 @@ python src/benchmark/RespLLM/RespLLM.py \
         --train_tasks S1,S2,S3,S4,S5,S6,S7 \
         --test_tasks T1,T2,T3,T4,T5,T6 \
         --train_epochs 40 \
-        --meta_val_interval 3  \
+        --meta_val_interval 3 \
         --train_pct 1 \
-        --batch_size 16 \
+        --batch_size $BATCH_SIZE \
         2>&1 | tee -a logs_1/out_RespLLM_gemma2B.txt
 
 # with audio label
@@ -19,8 +21,8 @@ python src/benchmark/RespLLM/RespLLM.py \
         --train_tasks S1,S2,S3,S4,S5,S6,S7 \
         --test_tasks T1,T2,T3,T4,T5,T6 \
         --train_epochs 40 \
-        --meta_val_interval 3  \
+        --meta_val_interval 3 \
         --train_pct 1 \
-        --batch_size 16 \
+        --batch_size $BATCH_SIZE \
         --use_audiolabel \
         2>&1 | tee -a logs_1/out_RespLLM_gemma2B_audiolabel.txt
