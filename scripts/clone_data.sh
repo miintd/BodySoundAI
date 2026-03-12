abbf17aea7b853518f4dca88ba0fe9d7a787bd21
Quoc Khanh <trandamquockhanh123@gmail.com>
2026-03-12 18:01:25 +0700

    update

--- scripts/clone_data.sh
+++ scripts/clone_data.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# install git lfs and clone the dataset
-git lfs install
-git clone --depth 1 https://huggingface.co/datasets/diemmii/opera-data		
-cd opera-data
-git sparse-checkout init --cone
-git sparse-checkout set datasets
-git pull
-mv datasets ../
-cd ..
-echo "Dataset cloned successfully."
-
-cd opera-data
-git sparse-checkout set feature
-git pull
-mv feature ../
-cd ..
-echo "Feature cloned successfully."
+# clone the dataset
+wget https://huggingface.co/datasets/diemmii/opera-data/resolve/main/opera_dataset.zip
+unzip opera_dataset.zip
