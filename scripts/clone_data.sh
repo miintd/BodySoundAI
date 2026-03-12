895762d3c7e284ccf1623f50df050b0e44e62354
Quoc Khanh <trandamquockhanh123@gmail.com>
2026-03-12 21:47:03 +0700

    update

--- scripts/clone_data.sh
+++ scripts/clone_data.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!bin/bash
+
+# remove old folders if they exist
+[ -d "datasets" ] && rm -rf datasets
+[ -d "feature" ] && rm -rf feature
 
 # clone the dataset
 wget https://huggingface.co/datasets/diemmii/opera-data/resolve/main/opera_dataset.zip
